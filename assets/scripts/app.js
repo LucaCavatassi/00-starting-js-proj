8624f601ac892b308613dc2244ec83abfc100b06
LucaCavatassi <lucamariacavatassi@gmail.com>
2024-09-03 11:01:01 +0200

    feat: import export options

---
+++ assets/scripts/app.js
@@ -0,0 +1,11 @@
+// import { apiKey } from "./util.js";
+
+// import apiKey  from "./util.js";
+
+// import { abc, bce } from "./util.js"
+
+// console.log(abc,bce);
+
+import * as util from "./util.js" 
+console.log(util.abc, util.bce, util.default);
+
---
+++ assets/scripts/util.js
@@ -0,0 +1,8 @@
+// export let apiKey = "aapdada"
+
+// export default "adnsdadda123";
+
+export default "adnsdadda123";
+export let abc = "aapdada";
+export let bce = "123123" 
+
