9271b68ed14d6c4ce779b0a7b8d765b64fa61b3d
LucaCavatassi <lucamariacavatassi@gmail.com>
2024-09-03 12:13:07 +0200

    feat: destructuring

--- assets/scripts/app.js
+++ assets/scripts/app.js
@@ -34,23 +34,58 @@
 // console.log(user1);
 // user1.greetings();
 
+// ARRAY METHODS RECAP
 const hobbies = ["sport", "music", "cinema"]
 hobbies.push("writing")
 
 const index = hobbies.findIndex((item)=> item === "writing") 
-console.log(index);
+// console.log(index);
 
 const textTransform = hobbies.map((item) => item + "!")
-console.log(textTransform);
+// console.log(textTransform);
 
 const textTypeTransform = hobbies.map((item) => ({passion: item}) )
-console.log(textTypeTransform);
+// console.log(textTypeTransform);
 
 for (let i = 0; i < hobbies.length; i++) {
-    console.log(hobbies[i]);
+    // console.log(hobbies[i]);
 }
 
 for (let i = hobbies.length - 1; i >= 0; i--) {
-    console.log(hobbies[i]);
+    // console.log(hobbies[i]);
     
 }
+
+// DESTRUCTURING
+// const array = ["luca", "fabio"];
+
+// const user1 = array[0];
+// const user2 = array[1];
+
+// console.log(user1, user2);
+
+// con il destructuring posso fare così
+
+const [user1, user2] = ["luca", "fabio"];
+console.log(user1, user2);
+
+// si può usare anche per gli oggetti
+
+// const user = {
+//     name: "Max", 
+//     age: 34
+// }
+
+// const name = user.name
+// const age = user.age
+
+// console.log(name, age);
+
+// destrutturato
+const {name, age} = {
+    name: "Max", 
+    age: 34
+}
+// il nome deve essere uguale alla key
+console.log(name, age);
+
